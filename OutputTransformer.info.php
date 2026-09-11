a5851f462afcd3b391f0b3e6158323e8b415bba8
theoretic <tarasov.alexei@gmail.com>
2026-09-11 11:28:06 +0300

    Update version number and date in module info and main file

--- OutputTransformer.info.php
+++ OutputTransformer.info.php
@@ -2,14 +2,14 @@
 /*
 Output Transformer module info
 AT
-25.02.26
+13.08.26
 */
 
 namespace ProcessWire;
 
 $info = [
 	'title'					=> 'OutputTransformer',
-	'version'				=> 021,
+	'version'				=> '0.3.0',
 	'summary'				=> 'Strips extra symbols from output, performs typographics, replacements etc.',
 	'author'				=> 'AT / atis.pro',
 	'href'					=> 'http://atis.pro',
